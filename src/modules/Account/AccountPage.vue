329548120584791be304c7d75ac163c86785b123
ezy4me <mrronal@mail.ru>
2023-11-25 16:42:57 +0300

    temporary fix account page scroll

--- src/modules/Account/AccountPage.vue
+++ src/modules/Account/AccountPage.vue
@@ -1,12 +1,18 @@
 <template>
   <q-page class="my-page q-py-md">
-    <div v-if="$q.screen.gt.sm" class="fit row justify-center items-start content-start">
+    <div
+      v-if="$q.screen.gt.sm"
+      class="fit row justify-center items-start content-start">
       <v-account-side-bar />
       <router-view></router-view>
     </div>
-    <div v-if="!$q.screen.gt.sm" class="fit col justify-center items-start content-start">
-      <v-account-side-bar />
-      <router-view></router-view>
+    <div
+      v-if="!$q.screen.gt.sm"
+      class="fit col justify-center items-start content-start">
+      <q-scroll-area class="col my-scroll">
+        <v-account-side-bar />
+        <router-view></router-view>
+      </q-scroll-area>
     </div>
   </q-page>
 </template>
